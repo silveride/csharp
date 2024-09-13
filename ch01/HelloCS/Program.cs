a8be19a419ee3401f6fc588c7be51e5b8237048e
silveride <manu.formal@gmail.com>
2024-09-14 08:17:36 +1000

    Single and multiproject solutions

---
+++ ch01/HelloCS/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = typeof(Program).Namespace ?? "None!";
+Console.WriteLine($"Namespace : {name}");
+throw new Exception();
